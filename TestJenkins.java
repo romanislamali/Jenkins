425d1fff44a7bfcf032e723a55f00e7a352a0d6f
Md. Roman Islam Ali <romanislam897@gmail.com>
2024-10-26 22:16:13 +0600

    Initial commit

---
+++ TestJenkins.java
@@ -0,0 +1,9 @@
+public class TestJenkins {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first Jenkins project:");
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
