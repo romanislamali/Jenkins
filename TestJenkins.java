9dd03619a9fbb60e6335cf5f7242a2aaa914a596
Md. Roman Islam Ali <romanislam897@gmail.com>
2024-10-26 22:50:52 +0600

    Write new print

--- TestJenkins.java
+++ TestJenkins.java
@@ -4,6 +4,7 @@ public class TestJenkins {
         System.out.println("This is my first Jenkins project:");
         for(int i=1; i<=10; i++){
             System.out.println(i);
+            System.out.println("This is new added line");
         }
     }
 }
