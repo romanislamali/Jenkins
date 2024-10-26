260fe118de497fb1eeeecdb3633d281a659ec1ae
Md. Roman Islam Ali <romanislam897@gmail.com>
2024-10-26 23:03:10 +0600

    Create new object and call static method

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public HelloWorld(){
+        System.out.println("Creating new HelloWorld object");
+    }
+    
+    public void hello(){
+        System.out.println("Hello java from HelloWorld");
+    }
+
+    public static int doSum(){
+        int result = 25 + 24;
+        return result;
+    }
+}
--- TestJenkins.java
+++ TestJenkins.java
@@ -1,9 +1,8 @@
 public class TestJenkins {
     public static void main(String[] args) {
         System.out.println("This is my first Jenkins project:");
-        for(int i=1; i<=10; i++){
-            System.out.println(i);
-            System.out.println("This is new added line");
-        }
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.hello();
+        System.out.println("The result of the sum is: "+ HelloWorld.doSum());
     }
 }
